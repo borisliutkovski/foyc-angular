1128880fa02c8d667b1c02b7e8b037c5839bde8b
Barys Liutkouski <boris.liutkovski@gmail.com>
2019-02-26 00:34:54 +0300

    upd

--- src/app/routes/rxjs-test/upsert/test-2.component.ts
+++ src/app/routes/rxjs-test/upsert/test-2.component.ts
@@ -14,3 +14,49 @@ export class Test2Component {
     }
   }
 }
+
+// import { Component } from '@angular/core'
+// import { LocalNewsService } from '../../home/local-news/local-news.service'
+
+// @Component({
+//   selector: 'app-test-2',
+//   template: ``,
+// })
+// export class Test2Component {
+//   constructor(private localN: LocalNewsService) {
+//     // set production to true in env.ts
+
+//     for (let i = 0; i < 10000; i++) {
+//       // creating a gazillion subscriptions
+//       // once the component is destroyed by an ngIf, memory should free up
+//       localN.getTest().toPromise()
+//     }
+//   }
+// }
+
+// import { Component, OnDestroy } from '@angular/core'
+// import { LocalNewsService } from '../../home/local-news/local-news.service'
+// import { Subscription } from 'rxjs';
+
+// @Component({
+//   selector: 'app-test-2',
+//   template: ``,
+// })
+// export class Test2Component implements OnDestroy {
+//   private subs: Subscription[] = []
+//   constructor(private localN: LocalNewsService) {
+//     // set production to true in env.ts
+
+//     for (let i = 0; i < 10000; i++) {
+//       // creating a gazillion subscriptions
+//       // once the component is destroyed by an ngIf, memory should free up
+//       this.subs.push(localN.getTest().subscribe())
+//     }
+//   }
+
+//   ngOnDestroy() {
+//     for (let sub of this.subs) {
+//       sub.unsubscribe()
+//     }
+//   }
+// }
